37fda92555a60c0772e748cdfb2defb39b8f4cb9
rohitsingla721 <99911820+rohitsingla721@users.noreply.github.com>
2025-05-19 17:42:25 +0530

    Update dijaktra.cpp

--- dijaktra.cpp
+++ dijaktra.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    
+    // Graph represented as an adjacency matrix (5 nodes)
     int g[5][5] = {
         {0, 10, 0, 5, 0},
         {0, 0, 1, 2, 0},
@@ -11,35 +11,42 @@ int main() {
         {7, 0, 6, 0, 0}
     };
 
-    int d[5];             
-    int v[5] = {};        
-    int n = 5;            
-    int INF = 1e9;        
+    int d[5];             // Stores shortest distances from node 0
+    int v[5] = {};        // Tracks if a node is visited (0 = no, 1 = yes)
+    int n = 5;            // Number of nodes
+    int INF = 1e9;        // A large number to represent infinity
 
     // Step 1: Initialize distances
-    for (int i = 0; i < n; i++)
-        d[i] = INF;       
-    d[0] = 0;             
-
- 
     for (int i = 0; i < n; i++) {
-        int u = -1;       
+        d[i] = INF;       // Start with all distances as infinity
+    }
+    d[0] = 0;             // Distance to self (node 0) is 0
 
-      
-        for (int j = 0; j < n; j++)
-            if (!v[j] && (u == -1 || d[j] < d[u]))
+    // Step 2: Main loop to find shortest path
+    for (int i = 0; i < n; i++) {
+        int u = -1;       // u will be the next node with smallest distance
+
+        // Find the unvisited node with the smallest distance
+        for (int j = 0; j < n; j++) {
+            if (!v[j] && (u == -1 || d[j] < d[u])) {
                 u = j;
+            }
+        }
 
-        v[u] = 1;        
+        v[u] = 1;         // Mark node u as visited
 
-      
-        for (int j = 0; j < n; j++)
-            if (g[u][j] && d[u] + g[u][j] < d[j])
+        // Update distances to neighbors of node u
+        for (int j = 0; j < n; j++) {
+            if (g[u][j] && d[u] + g[u][j] < d[j]) {
                 d[j] = d[u] + g[u][j];
+            }
+        }
     }
 
-  
-    for (int i = 0; i < n; i++)
+    // Step 3: Print shortest distances from node 0
+    for (int i = 0; i < n; i++) {
         cout << "Shortest distance from 0 to " << i << " = " << d[i] << endl;
+    }
+
     return 0;
 }
