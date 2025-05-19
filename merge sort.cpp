9c5e7eb8a702ab8296b769fe4e85872ad83ffe6d
rohitsingla721 <99911820+rohitsingla721@users.noreply.github.com>
2025-05-19 17:43:11 +0530

    Update merge sort.cpp

--- merge sort.cpp
+++ merge sort.cpp
@@ -1,21 +1,32 @@
-#include <iostream>
+#include <iostream>  
 using namespace std;
 
 void merge(int a[], int l, int m, int r) {
     int i = l, j = m + 1, b[100], k = 0;
-    
+
+    // Merge the two halves of the array
     while (i <= m && j <= r) {
-        if (a[i] < a[j])
+        if (a[i] < a[j]) {
             b[k++] = a[i++];
-        else
+        } else {
             b[k++] = a[j++];
+        }
     }
 
-    while (i <= m) b[k++] = a[i++];
-    while (j <= r) b[k++] = a[j++];
+    // Copy remaining elements from left subarray
+    while (i <= m) {
+        b[k++] = a[i++];
+    }
 
-    for (int x = 0; x < k; x++)
+    // Copy remaining elements from right subarray
+    while (j <= r) {
+        b[k++] = a[j++];
+    }
+
+    // Copy merged elements back to original array
+    for (int x = 0; x < k; x++) {
         a[l + x] = b[x];
+    } 
 }
 
 void mergeSort(int a[], int l, int r) {
@@ -30,9 +41,12 @@ void mergeSort(int a[], int l, int r) {
 int main() {
     int a[] = {5, 2, 4, 1, 3};
     int n = 5;
-    
+
     mergeSort(a, 0, n - 1);
-    
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
+    }
+
+    return 0;
 }
